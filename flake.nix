c40cb8b57f751b77b4fcfe540aff83c1b1a75268
luccahuguet <lucca.huguet2@gmail.com>
2026-06-03 11:25:48 -0300

    Avoid cargo build hook for wasm package

--- flake.nix
+++ flake.nix
@@ -36,11 +36,10 @@
           cargoLock.lockFile = ./Cargo.lock;
           nativeBuildInputs = [ pkgs.pkg-config ];
           buildInputs = [ pkgs.openssl ];
+          dontCargoBuild = true;
           doCheck = false;
 
           buildPhase = ''
-            runHook preBuild
-
             export CARGO="${rustToolchain}/bin/cargo"
             export RUSTC="${rustToolchain}/bin/rustc"
             export PATH="${rustToolchain}/bin:$PATH"
@@ -50,6 +49,8 @@
               echo "Rust toolchain is missing wasm32-wasip1 std at $wasm_target_libdir" >&2
               exit 1
             fi
+
+            runHook preBuild
 
             "$CARGO" build \
               --target-dir target \
